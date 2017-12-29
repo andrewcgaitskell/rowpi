7aa0a25ad0746d2c39b76ad7023920ee5f1ff185
andrewcgaitskell <andrew@gaitskell.com>
2017-12-29 16:33:16 +0000

    Update strokelog.py

--- strokelog.py
+++ strokelog.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
         #Loop while waiting for drive
         while forceplot['strokestate'] != 2 and workout['state'] == 1:
             #ToDo: sleep?
-            #forceplot = erg.get_force_plot()
+            forceplot = erg.get_force_plot()
             workout = erg.get_workout()
 
         #Record force data during the drive
