0622e30256fb8c36e563dec09e0b383c87507897
andrewcgaitskell <andrew@gaitskell.com>
2017-12-30 19:42:29 +0000

    Update flaskbokehplot.py

--- flaskbokehplot.py
+++ flaskbokehplot.py
@@ -1,45 +1,23 @@
-from flask import Flask, render_template, request
-import pandas as pd
-from bokeh.charts import Histogram
-from bokeh.embed import components
+import time
+from random import shuffle
+from bokeh.plotting import figure, output_server, cursession, show
 
-app = Flask(__name__)
+# prepare output to server
+output_server("animated_line")
 
-# Load the Iris Data Set
-iris_df = pd.read_csv("data/iris.data", 
-    names=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Species"])
-feature_names = iris_df.columns[0:-1].values.tolist()
+p = figure(plot_width=400, plot_height=400)
+p.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], name='ex_line')
+show(p)
 
-# Create the main plot
-def create_figure(current_feature_name, bins):
-	p = Histogram(iris_df, current_feature_name, title=current_feature_name, color='Species', 
-	 	bins=bins, legend='top_right', width=600, height=400)
+# create some simple animation..
+# first get our figure example data source
+renderer = p.select(dict(name="ex_line"))
+ds = renderer[0].data_source
 
-	# Set the x axis label
-	p.xaxis.axis_label = current_feature_name
+while True:
+    # Update y data of the source object
+    shuffle(ds.data["y"])
 
-	# Set the y axis label
-	p.yaxis.axis_label = 'Count'
-	return p
-
-# Index page
-@app.route('/')
-def index():
-	# Determine the selected feature
-	current_feature_name = request.args.get("feature_name")
-	if current_feature_name == None:
-		current_feature_name = "Sepal Length"
-
-	# Create the plot
-	plot = create_figure(current_feature_name, 10)
-		
-	# Embed plot into HTML via Flask Render
-	script, div = components(plot)
-	return render_template("iris_index1.html", script=script, div=div,
-		feature_names=feature_names,  current_feature_name=current_feature_name)
-
-# With debug=True, Flask server will auto-reload 
-# when there are code changes
-if __name__ == '__main__':
-	app.run(host='0.0.0.0',port=5000, debug=True)
-
+    # store the updated source on the server
+    cursession().store_objects(ds)
+    time.sleep(0.5)
