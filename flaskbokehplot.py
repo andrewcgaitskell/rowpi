4407d53b6237353be996405d7cc4369f2f1c9fe5
andrewcgaitskell <andrew@gaitskell.com>
2017-12-30 19:24:32 +0000

    Update flaskbokehplot.py

--- flaskbokehplot.py
+++ flaskbokehplot.py
@@ -41,4 +41,5 @@ def index():
 # With debug=True, Flask server will auto-reload 
 # when there are code changes
 if __name__ == '__main__':
-	app.run(port=5000, debug=True)
+	app.run(host='0.0.0.0',port=5000, debug=True)
+
