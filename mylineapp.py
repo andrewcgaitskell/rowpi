5eed55eac0bc6a70eaa6df78101455135e65067f
andrewcgaitskell <andrew@gaitskell.com>
2017-12-30 21:13:49 +0000

    Update mylineapp.py

--- mylineapp.py
+++ mylineapp.py
@@ -8,11 +8,17 @@ from bokeh.palettes import RdYlBu3
 from bokeh.plotting import figure, curdoc
 
 # create a plot and style its properties
-p = figure(x_range=(0, 100), y_range=(0, 100), toolbar_location=None)
-p.border_fill_color = 'black'
-p.background_fill_color = 'black'
-p.outline_line_color = None
-p.grid.grid_line_color = None
+p = figure(plot_width=400, plot_height=400,x_range=(0, 100), y_range=(0, 100), toolbar_location=None)
+p.border_fill_color = 'white'
+p.background_fill_color = 'red'
+p.outline_line_color = 'black'
+p.grid.grid_line_color = 'black'
+
+# add a line renderer
+p.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], line_width=2)
+
+p.multi_line([[1, 3, 2], [3, 4, 6, 6]], [[2, 1, 4], [4, 7, 8, 5]],
+             color=["firebrick", "navy"], alpha=[0.8, 0.3], line_width=4)
 
 # add a text renderer to our plot (no data yet)
 r = p.text(x=[], y=[], text=[], text_color=[], text_font_size="20pt",
