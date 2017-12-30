3e782096f783d2b40d87ce16af8be163799143a8
andrewcgaitskell <andrew@gaitskell.com>
2017-12-30 11:15:21 +0000

    Update testodbc.py

--- testodbc.py
+++ testodbc.py
@@ -1,5 +1,11 @@
 import psycopg2
 
+from sqlalchemy import create_engine
+
+import pandas as pd
+
+from pandas.io import sql
+
 conn = psycopg2.connect('host=localhost user=andrew password=andrew dbname=test')
 
 cur = conn.cursor()
@@ -10,3 +16,16 @@ results = cur.fetchall()
 
 for result in results:
     print(result)
+
+cur.close()
+conn.close()    
+
+####################### 
+
+engine = create_engine('postgresql://andrew:andrew@localhost:5432/data')
+
+sql.execute('SELECT stroketime, distance, spm, power, pace, calhr, calories, heartrate, status, rowingid FROM strokes.floats;', engine)
+
+
+
+pd.read_sql_query('SELECT * FROM data', engine)
