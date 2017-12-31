f4636e125ce1ffd58ef89f38800ed9b41a67c67e
andrewcgaitskell <andrew@gaitskell.com>
2017-12-31 13:36:05 +0000

    Update mylineapp.py

--- mylineapp.py
+++ mylineapp.py
@@ -106,7 +106,7 @@ plot = Plot(
     h_symmetry=False, v_symmetry=False, min_border=0, toolbar_location=None)
 
 
-glyph2 = MultiLine('stroketime','distance',source = source2, line_color = 'colorofline')
+glyph2 = MultiLine('stroketime','distance',source = source, line_color = 'colorofline')
 
 #glyph = MultiLine(xs="stroketime", ys="distance", line_color="#8073ac", line_width=2)
 
