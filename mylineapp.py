d0bc5d8beb71ef5137ff271230a93925d8db4bc8
andrewcgaitskell <andrew@gaitskell.com>
2017-12-31 19:23:10 +0000

    Update mylineapp.py

--- mylineapp.py
+++ mylineapp.py
@@ -110,6 +110,9 @@ source = ColumnDataSource(data=df)
 
 print source
 
+plot.multi_line([[1, 3, 2], [3, 4, 6, 6]], [[2, 1, 4], [4, 7, 8, 5]],
+         color=["firebrick", "navy"], alpha=[0.8, 0.3], line_width=4)
+
 glyph = MultiLine(xs='stroketime', ys='distance', line_color='#3288bd', line_width=6)
 
 #plot.add_glyph(source, glyph)
