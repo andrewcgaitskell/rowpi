919fd2e49e351a5dfb31e7d0951e272650109ec7
andrewcgaitskell <andrew@gaitskell.com>
2018-10-20 12:42:43 +0100

    Update ReadHR.py

--- ReadHR.py
+++ ReadHR.py
@@ -49,8 +49,10 @@ HeartService=p.getServiceByUUID(BLE_SERVICE_UUID)
 try:
    ch = HeartService.getCharacteristics(BLE_CHARACTERISTIC_UUID)
    while 1:
-      val = binascii.b2a_hex(ch.read())
-      print ("0x" + val)
+      #val = binascii.b2a_hex(ch.read())
+      #print ("0x" + val)
+      h = ch.read()
+      print(h)
       time.sleep(1)
 
 finally:
