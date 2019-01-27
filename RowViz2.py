c756eff767bbe4ac628ce498904b7c5af0214d41
andrewcgaitskell <andrew@gaitskell.com>
2019-01-27 17:57:26 +0000

    Update RowViz2.py

--- RowViz2.py
+++ RowViz2.py
@@ -28,7 +28,7 @@ engine = create_engine('postgresql://pi:raspberry@localhost:5432/rowingdata')
 
 # sqlcmnd = 'SELECT stroketime, distance,rowingid FROM data.strokes;'
 
-sqlcmnd = 'select row_number() OVER(PARTITION BY rowingid ORDER BY distance) strokecounter, stroketime, distance,(pace/60) as pace,  rowingid FROM data.strokes;'
+sqlcmnd = 'select row_number() OVER(PARTITION BY rowingid ORDER BY distance) strokecounter, stroketime, distance,cast(pace as Double precision)/60 as pace, 4*cast(pace as Double precision)/60 as TwoKEstimate, rowingid FROM data.strokes;'
 
 sqlcmnd_heart = 'select row_number() OVER(PARTITION BY rowingid ORDER BY distance) strokecounter,heartrate, rowingid FROM data.strokes;'
 
@@ -64,7 +64,7 @@ xs = [df_out.loc[df_out['rowingid'] == i].strokecounter for i in grp_list]
 print("xs",xs)
 
 
-ys = [df_out.loc[df_out['rowingid'] == i].pace for i in grp_list]
+ys = [df_out.loc[df_out['rowingid'] == i].TwoKEstimate for i in grp_list]
 
 ys_heart = [df_out_heart.loc[df_out_heart['rowingid'] == i].heartrate for i in grp_list]
 
@@ -86,7 +86,7 @@ p3 = figure(plot_width=600, plot_height=400)
 
 p4 = figure(plot_width=600, plot_height=400)
 
-p3.yaxis.axis_label = 'pace'
+p3.yaxis.axis_label = 'TwoKEstimate'
 p3.yaxis.axis_label_text_font_size = '30pt'
 p3.yaxis.major_label_text_font_size = '25pt'
 
