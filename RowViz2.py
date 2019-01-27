44b4f13df95ab5ab0abc8b8fe2450abb7a465b69
andrewcgaitskell <andrew@gaitskell.com>
2019-01-27 15:09:28 +0000

    Create RowViz2.py

---
+++ RowViz2.py
@@ -0,0 +1,83 @@
+from bokeh.layouts import column
+
+from bokeh.models import Plot
+
+from bokeh.models import Grid
+from bokeh.models import DataRange1d
+from bokeh.models import ColumnDataSource
+
+from bokeh.palettes import Spectral11
+from bokeh.plotting import figure, curdoc
+from bokeh.server.server import Server
+from bokeh.themes import Theme
+
+from bokeh.models.glyphs import Line
+from bokeh.io import curdoc, show
+from bokeh.plotting import reset_output
+
+from sqlalchemy import create_engine
+
+import pandas as pd
+
+engine = create_engine('postgresql://pi:raspberry@localhost:5432/rowingdata')
+
+#sqlcmnd = 'SELECT stroketime, distance, spm, power, pace, calhr, calories, heartrate, status, rowingid FROM strokes.floats;'
+
+# sqlcmnd = 'SELECT stroketime, distance,rowingid FROM data.strokes;'
+
+sqlcmnd = 'select row_number() OVER(PARTITION BY rowingid ORDER BY distance) strokecounter, stroketime, distance,pace,  rowingid FROM data.strokes;'
+
+df_out = pd.read_sql_query(sqlcmnd, engine)
+
+newx_out = []
+newy_out = []
+
+# Group the dataframe by regiment, and for each regiment,
+for stroketime, group in df_out.groupby('rowingid'): 
+    my_group_times = group['stroketime'].tolist()
+    newx_out.append(my_group_times)
+
+for distance, group in df_out.groupby('rowingid'): 
+    my_group_distances = group['distance'].tolist()
+    newy_out.append(my_group_distances)
+
+listofrows_out = df_out['rowingid'].unique()
+
+numlines=len(listofrows_out)
+
+mypalette_out=Spectral11[0:numlines]
+
+xdr = DataRange1d()
+ydr = DataRange1d()
+
+######## bokeh - multi_line #############
+grp_list = df_out['rowingid'].unique()
+
+rowscount = len(grp_list)
+
+simplelegend = []
+
+for r in range(0,rowscount):
+    simplelegend.append(r)
+
+
+xs = [df_out.loc[df_out['rowingid'] == i].strokecounter for i in grp_list]
+print("xs",xs)
+
+
+ys = [df_out.loc[df_out['rowingid'] == i].pace for i in grp_list]
+source = ColumnDataSource(data=dict(
+     x = xs,
+     y = ys,
+     color = mypalette_out,
+     group = simplelegend))
+p3 = figure(plot_width=1000, plot_height=600)
+p3.multi_line(
+     xs='x',
+     ys='y',
+     legend='group',
+     source=source,
+     line_color='color')
+
+show(p3)
+
