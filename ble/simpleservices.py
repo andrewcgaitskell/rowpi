d1108a7cdb4eff8b8a2ae7ba2d271c8d54609f59
andrewcgaitskell <andrew@gaitskell.com>
2020-03-27 17:00:18 +0000

    Update simpleservices.py

--- ble/simpleservices.py
+++ ble/simpleservices.py
@@ -3,6 +3,13 @@ from bluepy import btle
 print("Connecting...")
 dev = btle.Peripheral("FA:D0:B8:89:AE:1F", btle.ADDR_TYPE_RANDOM)
 
+characteristicslist = dev.getCharacteristics()
+
+
 print("Services...")
 for svc in dev.services:
     print(str(svc))
+
+print("Characteristics...")
+for cl in characteristicslist:
+    print(str(cl))
