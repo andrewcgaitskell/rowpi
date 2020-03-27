e764630a32fc24b4e0c6356bdff7ee3dd85a351b
andrewcgaitskell <andrew@gaitskell.com>
2020-03-27 15:56:01 +0000

    Update simpleservices.py

--- ble/simpleservices.py
+++ ble/simpleservices.py
@@ -1,7 +1,7 @@
 from bluepy import btle
  
 print("Connecting...")
-dev = btle.Peripheral("FA:D0:B8:89:AE:1F")
+dev = btle.Peripheral("FA:D0:B8:89:AE:1F", btle.ADDR_TYPE_RANDOM)
 
 print("Services...")
 for svc in dev.services:
