80ca86f7de0527792bd8409343b864616a9be0ef
andrewcgaitskell <andrew@gaitskell.com>
2020-03-27 21:42:24 +0000

    Update simpleservices.py

--- ble/simpleservices.py
+++ ble/simpleservices.py
@@ -1,10 +1,16 @@
 from bluepy import btle
- 
+
+from bluepy.btle import UUID, Peripheral, DefaultDelegate
+
+
 print("Connecting...")
 dev = btle.Peripheral("FA:D0:B8:89:AE:1F", btle.ADDR_TYPE_RANDOM)
 
 characteristicslist = dev.getCharacteristics()
 
+CSC_SERVICE_UUID = UUID(0x1816)
+CSC_CHAR_UUID = UUID(0x2A5B)
+LOCATION_CHAR_UUID = UUID(0x2A5D)
 
 print("Services...")
 for svc in dev.services:
