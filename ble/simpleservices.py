6cc0912a2fc4d682286c2cea45dcb711ff6215c1
andrewcgaitskell <andrew@gaitskell.com>
2020-03-27 15:51:14 +0000

    Update simpleservices.py

--- ble/simpleservices.py
+++ ble/simpleservices.py
@@ -1,8 +1,8 @@
 from bluepy import btle
  
- print("Connecting...")
- dev = btle.Peripheral("FA:D0:B8:89:AE:1F")
- 
- print("Services...")
- for svc in dev.services:
-     print(str(svc))
+print("Connecting...")
+dev = btle.Peripheral("FA:D0:B8:89:AE:1F")
+
+print("Services...")
+for svc in dev.services:
+    print(str(svc))
