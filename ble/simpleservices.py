18023b615949ddad9b5f5eac97d5f1e46ee3421c
andrewcgaitskell <andrew@gaitskell.com>
2020-03-28 12:16:53 +0000

    Update simpleservices.py

--- ble/simpleservices.py
+++ ble/simpleservices.py
@@ -8,7 +8,7 @@ print("Connecting...")
 
 #dev = btle.Peripheral("FA:D0:B8:89:AE:1F", btle.ADDR_TYPE_RANDOM) # cadence meter
 
-dev = btle.Peripheral("30:AE:A4:83:F6:46", btle.ADDR_TYPE_RANDOM) # test beacon
+dev = btle.Peripheral("30:AE:A4:83:F6:46", btle.ADDR_TYPE_PUBLIC) # test beacon
 
 characteristicslist = dev.getCharacteristics()
 
